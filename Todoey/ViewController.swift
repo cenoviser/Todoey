7dbb449ef4d4bac455ed8c5e9c3b5818e17f2dc0
Jiwoo Ban <jban365@gmail.com>
2018-12-07 20:20:26 +0900

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Jiwoo Ban on 12/7/18.
+//  Copyright © 2018 Jiwoo Ban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
